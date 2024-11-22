d755aef7297af98d37cb1a2d5c12a90fc990d831
MARKAPURAM VENKATA AKHIL <122592444+Akhil244@users.noreply.github.com>
2024-11-23 00:46:56 +0530

    implementing setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='Sensor',
+    version='0.0.1',
+    author="Akhil",
+    author_email='markapuramakhil@gmail.com',
+    packages=find_packages(),
+    install_requires=['pymongo']
+)
