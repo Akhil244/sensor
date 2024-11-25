c693d1f9afa4d422ab075f8aa7d986181d0adbd0
MARKAPURAM VENKATA AKHIL <122592444+Akhil244@users.noreply.github.com>
2024-11-25 21:58:27 +0530

    after adding editable mode e

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages,setup
 
 
+def get_requirements() -> list[str]:
+    requirements_list=list[str]=[]
+
+    return requirements_list
+
 setup(
     name='Sensor',
     version='0.0.1',
     author="Akhil",
     author_email='markapuramakhil@gmail.com',
     packages=find_packages(),
-    install_requires=['pymongo']
+    install_requires=get_requirements()
 )
